1b85b8a4b09ec1ffe38c2a5994fe9a4eb966539c
clfee <>
2024-06-11 22:57:01 +0800

    no message

---
+++ MpcLibCoreSdkDemo/MpcLibCoreSdkDemo/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  MpcLibCoreSdkDemo
+//
+//  Created by 路飞 on 2024/6/9.
+//
+
+#import "ViewController.h"
+#import "Libmpccore/Libmpccore.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+    
+    NSString * rr =  MpccoreKeyGen(@"ecdsa");
+    
+}
+
+
+@end
